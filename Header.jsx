1005de9956c1b982cdd8a408f0b9fa820c0ac29b
Vimal-Thangaraju <84367257+Vimal-Thangaraju@users.noreply.github.com>
2021-06-05 20:04:29 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <h1>Shape AI</h1>
+      </div>
+    </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>Javascript and React.js</h1>
+      <p>Bootcamp Day</p>
+    </div>
+  );
+}
+export default Note;
